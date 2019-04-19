b341c19000553bb629deeabe0dbacaa2e7e77be2
Seth Baldridge <sethbaldridge87@gmail.com>
2019-04-19 13:25:39 -0600

    Updates

---
+++ precache-manifest.084171a3ec49a70180ebd057ed0859b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_deploy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df53b2a971eb4b5c53a3",
+    "url": "/react_deploy/static/js/runtime~main.36e202d2.js"
+  },
+  {
+    "revision": "132a6a60ea6631c6fc0b",
+    "url": "/react_deploy/static/js/main.4690d472.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react_deploy/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "132a6a60ea6631c6fc0b",
+    "url": "/react_deploy/static/css/main.9020f77f.chunk.css"
+  },
+  {
+    "revision": "5459395f68af4e808930bfeb69e500a5",
+    "url": "/react_deploy/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sethbaldridge87.github.io/react_deploy/precache-manifest.561c1db09ead24c34309a6235d592db9.js"
+  "/react_deploy/precache-manifest.084171a3ec49a70180ebd057ed0859b4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/sethbaldridge87.github.io/react_deploy/index.html", {
+workbox.routing.registerNavigationRoute("/react_deploy/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
